7620a89071a585326351e32ed5fc2b7c3a0e3ad4
zcf <czhe581@g.educaand.es>
2023-04-23 20:42:23 +0200

    upd

--- src/pages/api/food.ts
+++ src/pages/api/food.ts
@@ -1,15 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import prisma from "../../libs/prismadb";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import prisma from "../../libs/prismadb";
 
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const { name, description, price, image } = req.body;
-  const food = await prisma.food.create({
-    data: {
-      name,
-      description,
-      price,
-      image,
-    },
-  });
-  res.json(food);
-}
+// export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+//   const { name, description, price, image } = req.body;
+//   const food = await prisma.food.create({
+//     data: {
+//       name,
+//       description,
+//       price,
+//       image,
+//     },
+//   });
+//   res.json(food);
+// }
