e1f9a7ca752a1a85b3be919ae65e55254e1e7fc3
zcf <czhe581@g.educaand.es>
2023-05-07 09:59:15 +0200

    upd Deploy Fix

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -68,3 +68,25 @@ export default function LoginForm() {
         </div>
     );
 }
+
+
+// import { PrismaClient } from "@prisma/client";
+// import bcrypt from "bcrypt";
+
+// const prisma = new PrismaClient();
+
+// async function createAdmin() {
+//   const password = "toor"; // Contraseña
+//   const hashedPassword = await bcrypt.hash(password, 10);
+
+//   await prisma.admin.create({
+//     data: {
+//       username: "root", // NOmbre de usuario
+//       password: hashedPassword,
+//     },
+//   });
+
+//   console.log("Admin created");
+// }
+
+// createAdmin();
