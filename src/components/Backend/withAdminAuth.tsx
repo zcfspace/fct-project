e4247d2817061064ccaeb5db21911dd9b6b12ea7
zcf <czhe581@g.educaand.es>
2023-05-07 09:53:43 +0200

    upd FIx

--- src/components/Backend/withAdminAuth.tsx
+++ src/components/Backend/withAdminAuth.tsx
@@ -1,9 +1,10 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { ComponentType } from 'react';
 
-export default function withAdminAuth(PageComponent) {
-    return function AdminAuthComponent(props) {
+export default function withAdminAuth(PageComponent: ComponentType) {
+    return function AdminAuthComponent(props: JSX.IntrinsicAttributes) {
         const { data: session, status } = useSession();
         const router = useRouter();
 
