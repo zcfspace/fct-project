763e918ff444963a396af8423a5f8dfab98332fe
zcf <czhe581@g.educaand.es>
2023-05-18 21:40:39 +0200

    upd Fix

--- src/pages/api/reservations.ts
+++ src/pages/api/reservations.ts
@@ -1,11 +1,11 @@
-import prisma from "../../libs/prisma";
-import { Reservation } from "../../types/types";
+// import prisma from "../../libs/prisma";
+// import { Reservation } from "../../types/types";
 
-export const createReservation = async (reservation: Reservation) => {
-  const createdReservation = await prisma.reservation.create({
-    data: reservation,
-  });
+// export const createReservation = async (reservation: Reservation) => {
+//   const createdReservation = await prisma.reservation.create({
+//     data: reservation,
+//   });
 
-  return createdReservation;
-};
+//   return createdReservation;
+// };
 
