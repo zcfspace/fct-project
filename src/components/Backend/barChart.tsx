bbed24d4e3c0743a6ccab21dcedd6bae0216710a
zcf <czhe581@g.educaand.es>
2023-05-24 22:09:16 +0200

    upd dashboard chart

--- src/components/Backend/barChart.tsx
+++ src/components/Backend/barChart.tsx
@@ -1,11 +1,76 @@
-const BarChart = () => {
+import React, { useState, useEffect } from "react";
+import { Chart, BarController, BarElement, CategoryScale, LinearScale } from 'chart.js';
+import { format } from "date-fns";
+
+Chart.register(BarController, BarElement, CategoryScale, LinearScale);
+
+type OrderByDay = {
+  createdAt: string;
+  _count: {
+    createdAt: number;
+  };
+};
+
+function BarChart() {
+  const [ordersByDate, setOrdersByDate] = useState<OrderByDay[]>([]);
+  const [chartInstance, setChartInstance] = useState<Chart | null>(null);
+
+  const getOrdersByDate = async () => {
+    try {
+      const response = await fetch(`/api/dashboard/get`);
+      const data = await response.json();
+      setOrdersByDate(data.ordersByDay);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getOrdersByDate();
+  }, []);
+
+  useEffect(() => {
+    if (ordersByDate.length > 0) {
+      if (chartInstance) chartInstance.destroy();
+
+      const groupedOrders = ordersByDate.reduce<Record<string, number>>((acc, order) => {
+        const date = new Date(order.createdAt).toLocaleDateString();
+        if (!acc[date]) {
+          acc[date] = 0;
+        }
+        acc[date]++;
+        return acc;
+      }, {});
+
+      const labels = Object.keys(groupedOrders);
+      const data = Object.values(groupedOrders);
+
+      const ctx = document.getElementById("myChart") as HTMLCanvasElement;
+      const newChartInstance = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels,
+          datasets: [
+            {
+              label: "Orders by day",
+              data,
+            },
+          ],
+        },
+        options: {},
+      });
+
+      setChartInstance(newChartInstance as Chart);
+    }
+  }, [ordersByDate]);
+
   return (
     <>
-      <div className='w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white'>
-
+      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
+        <canvas id="myChart"></canvas>
       </div>
     </>
   );
-};
+}
 
 export default BarChart;
---
+++ src/pages/api/dashboard/get.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/libs/prisma";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const ordersByDay = await prisma.order.groupBy({
+        by: ["createdAt"],
+        _count: {
+          createdAt: true,
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      });
+
+      res.status(200).json({ ordersByDay });
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener los datos" });
+    }
+  } else {
+    res.status(405).json({ error: "Método no permitido" });
+  }
+}
