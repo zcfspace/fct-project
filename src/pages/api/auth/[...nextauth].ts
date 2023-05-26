b571710428b7ddd622ceec2c074623f769331f61
zcf <czhe581@g.educaand.es>
2023-05-26 17:31:55 +0200

    test fix

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -32,12 +32,17 @@ export const authOptions: AuthOptions = {
           credentials.password,
           admin.password
         );
-        
+
         if (!isCorrectPassword) {
           throw new Error("Contraseña incorrecta");
         }
+        console.log(admin);
 
-        return admin;
+        return {
+          id: admin.id,
+          username: admin.username,
+          email: admin.username + "@bambumexpress.com",
+        };
       },
     }),
   ],
@@ -47,107 +52,8 @@ export const authOptions: AuthOptions = {
   },
   session: {
     strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
   },
 };
 export default NextAuth(authOptions);
-
-// import NextAuth from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { PrismaClient } from "@prisma/client";
-
-// const prisma = new PrismaClient();
-
-// export default NextAuth({
-//   providers: [
-//     CredentialsProvider({
-//       name: "Credentials",
-//       credentials: {
-//         username: { label: "Username", type: "text" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials, req) {
-
-//         const { username, password } = credentials;
-
-//         const admin = await prisma.admin.findUnique({
-//           where: { username },
-//         });
-//         if (admin && admin.password === password) {
-//           return { id: admin.id, name: admin.name };
-//         } else {
-//           return null;
-//         }
-//       },
-//     }),
-//   ],
-//   // Aquí puedes agregar otras opciones y callbacks de NextAuth
-// });
-// import NextAuth from "next-auth";
-// import { PrismaClient } from "@prisma/client";
-// import bcrypt from "bcryptjs";
-// import CredentialsProvider from "next-auth/providers/credentials";
-
-// const prisma = new PrismaClient();
-
-// export default NextAuth({
-//   providers: [
-//     CredentialsProvider({
-//       name: "Credentials",
-//       credentials: {
-//         username: { label: "Username", type: "text" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorized(Credentials) {
-//         const admin = await prisma.admin.findUnique({
-//           where: { username: credentials.username },
-//         });
-
-//         if (admin) {
-//           const isPasswordValid = await bcrypt.compare(
-//             credentials.password,
-//             admin.password
-//           );
-//           if (isPasswordValid) {
-//             return { name: "Admin" };
-//           } else {
-//             return null;
-//           }
-//         } else {
-//           return null;
-//         }
-//       },
-//     }),
-//   ],
-// });
-
-// // callbacks: {
-// //   async jwt(token, admin) {
-// //     if (admin) {
-// //       token.name = admin.name;
-// //     }
-// //     return token;
-// //   },
-// //   async session(session, token) {
-// //     session.admin = token;
-// //     return session;
-// //   },
-// // },
-// // import NextAuth, { NextAuthOptions } from "next-auth";
-// // import GithubProvider from "next-auth/providers/github";
-
-// // export const authOptions: NextAuthOptions = {
-// //   providers: [
-// //     GithubProvider({
-// //       clientId: process.env.GITHUB_ID,
-// //       clientSecret: process.env.GITHUB_SECRET,
-// //     }),
-// //   ],
-// //   callbacks: {
-// //     async jwt({ token }) {
-// //       token.userRole = "admin";
-// //       return token;
-// //     },
-// //   },
-// // };
-
-// // export default NextAuth(authOptions);
--- src/pages/api/category/get.ts
+++ src/pages/api/category/get.ts
@@ -6,14 +6,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-
   const session = await getSession({ req });
 
   if (!session) {
     res.status(401).json({ error: "No autorizado" });
     return;
-  } 
-
+  }
+  
   if (req.method === "GET") {
     try {
       const categories = await prisma.category.findMany();
