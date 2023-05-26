2637b1d49d721cc2ef666b780eeb2f0fd80bb30a
zcf <czhe581@g.educaand.es>
2023-05-26 16:35:45 +0200

    test

--- src/pages/api/category/get.ts
+++ src/pages/api/category/get.ts
@@ -1,10 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/libs/prisma"
+import prisma from "@/libs/prisma";
+import { getSession } from "next-auth/react";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+
+  const session = await getSession({ req });
+
+  if (!session) {
+    res.status(401).json({ error: "No autorizado" });
+    return;
+  } 
+
   if (req.method === "GET") {
     try {
       const categories = await prisma.category.findMany();
